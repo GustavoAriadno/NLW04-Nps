4f272464fc1b1cab5c58670a644da078a777fc73
GustavoAriadno <gugu.gustavo2011@gmail.com>
2021-02-24 01:52:13 +0100

    Implement Data base

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
+import 'reflect-metadata'
 import express from "express";
+import './database';
+import { router } from './routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-	return response.json({ message: "Hello World - NLW04" });
-});
-
-app.post("/", (request, response) => {
-	// Save data
-	return response.json({ message: "The data has been saved" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
